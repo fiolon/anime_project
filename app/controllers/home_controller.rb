31bf96420d38c3be19a1746949f9fdc05553b021
fiolon <fnarciso@academic.rrc.ca>
2024-10-31 03:54:10 -0500

    Added styling to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @animes = Anime.includes(:studio)
+                   .order("score DESC")
+                   .limit(20)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "home/index"
+  root "home#index"
   get 'about', to: 'about#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
